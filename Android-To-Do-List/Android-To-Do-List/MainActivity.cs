44ede9584f8e557d8902c9db91e44a40a589b31b
DESKTOP-6ICSE8H\cddat <hareshdc@gmail.com>
2018-05-12 16:00:09 -0700

    project files

---
+++ Android-To-Do-List/Android-To-Do-List/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Android_To_Do_List
+{
+    [Activity(Label = "Android_To_Do_List", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
