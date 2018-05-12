6d205b131952a44942f63691e6e9af22a0998457
DESKTOP-6ICSE8H\cddat <hareshdc@gmail.com>
2018-05-12 16:01:08 -0700

    main layout design

--- Android-To-Do-List/Android-To-Do-List/Resources/Resource.Designer.cs
+++ Android-To-Do-List/Android-To-Do-List/Resources/Resource.Designer.cs
@@ -41,6 +41,28 @@ namespace Android_To_Do_List
 			}
 		}
 		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f040002
+			public const int AddButton = 2130968578;
+			
+			// aapt resource value: 0x7f040001
+			public const int itemText = 2130968577;
+			
+			// aapt resource value: 0x7f040000
+			public const int tableRow1 = 2130968576;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
+			{
+			}
+		}
+		
 		public partial class Layout
 		{
 			
